35d8e37c28d1b533dd7b70d991e9d28578e02575
NevesMN <neves.hubz@gmail.com>
2025-08-26 00:51:32 -0400

    Create github-user.js

---
+++ github-user.js
@@ -0,0 +1,19 @@
+// github-user.js
+const fetch = require("node-fetch");
+
+exports.handler = async function(event, context) {
+  const r = await fetch("https://api.github.com/user", {
+    headers: {
+      Authorization: `token ${process.env.GITHUB_TOKEN}`,
+      "User-Agent": "simple-proxy"
+    }
+  });
+
+  const data = await r.json();
+
+  return {
+    statusCode: r.status,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  };
+};
